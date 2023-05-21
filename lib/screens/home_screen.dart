be303b9e8f815ed76bf11e963a39de4f1b932a01
NetanelVinter <uogi100@gmail.com>
2023-05-21 15:09:46 +0300

    Add search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:workit/screens/businesses_feed.dart';
 import 'package:workit/screens/communities_feed.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -20,6 +19,14 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('WorkIt'),
+        actions: [
+          IconButton(
+              onPressed: () => showSearch(
+                    context: context,
+                    delegate: CustomSearchDelegate(),                                         
+                  ),
+              icon: const Icon(Icons.search))
+        ],
       ),
       body: IndexedStack(
         index: currentFeedIndex,
@@ -44,5 +51,89 @@ class _HomeScreenState extends State<HomeScreen> {
         },
       ),
     );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {    
+
+  List<String> SearchList = [
+    'ProFit',
+    'IClimb',
+    'Yoga',
+    'Basketball',
+    'Acro',
+    'Gym',
+    'Baseball',
+    'Football',
+    'CrossFit',
+  ];
+  
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {    
+    return [
+      IconButton(
+        onPressed: () => query = '',
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_outlined));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    if (query.isEmpty) {
+      return const Text('');
+    }
+
+    List<String> matchList = [];
+
+    for (String item in SearchList) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchList.add(item);
+      }
+    }
+
+    if (matchList.isNotEmpty) {
+      return ListView.builder(
+        itemCount: matchList.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(matchList[index]),
+          onTap: () {query = matchList[index];},
+        ),
+      );
+    } else {
+      return ListTile(
+        title: const Text('No result found'),
+        onTap: () {},
+      );
+    }
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchList = [];
+    for (String item in SearchList) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchList.add(item);
+      }
+    }    
+
+    return ListView.builder(
+        itemCount: matchList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(matchList[index]),
+            onTap: () {                               
+              query = matchList[index];                              
+            },
+          );
+        });
   }
 }
